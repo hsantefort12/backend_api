e745be0c526dd86634ae47f0157c322285debce7
Haden Santefort <hsantefort12@gmail.com>
2021-08-17 16:06:49 -0500

    test

--- src/Application.kt
+++ src/Application.kt
@@ -1,17 +1,11 @@
 package com.example
 
-import com.example.models.DatabaseController
 import com.example.routes.*
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.sessions.*
 import io.ktor.auth.*
 import io.ktor.gson.*
 import io.ktor.features.*
-import org.jetbrains.exposed.sql.Database
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -39,35 +33,6 @@ fun Application.module(testing: Boolean = false) {
     registerDatabaseRoutes()
     registerUserRoutes()
     registerCharacterRoutes()
-
-//    routing {
-//        get("/") {
-//            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-//        }
-//
-//        get("/create-db") {
-//            val db = DatabaseController()
-//            db.createTables()
-//            call.respondText("tables created", status = HttpStatusCode.Accepted)
-//        }
-//
-//        get("/session/increment") {
-//            val session = call.sessions.get<MySession>() ?: MySession()
-//            call.sessions.set(session.copy(count = session.count + 1))
-//            call.respondText("Counter is ${session.count}. Refresh to increment.")
-//        }
-//
-//        authenticate("myBasicAuth") {
-//            get("/protected/route/basic") {
-//                val principal = call.principal<UserIdPrincipal>()!!
-//                call.respondText("Hello ${principal.name}")
-//            }
-//        }
-//
-//        get("/json/gson") {
-//            call.respond(mapOf("hello" to "world"))
-//        }
-//    }
 }
 
 data class MySession(val count: Int = 0)
